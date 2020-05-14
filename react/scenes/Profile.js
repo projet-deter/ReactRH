0b7161ea804c88c674b2cf9ddfacc4b5999facd7
Maxime Amore <maxime.marco.amore@gmail.com>
2020-05-14 15:16:08 +0200

    2 petits fix

--- react/scenes/Profile.js
+++ react/scenes/Profile.js
@@ -1,6 +1,13 @@
 import React, {useState} from 'react';
 
-import {Text, View, Button, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
 import {Avatar, Title} from 'react-native-paper';
 import Form from 'react-native-basic-form';
 import CTA from '../components/CTA';
@@ -30,7 +37,7 @@ export default function Home(props) {
     initialData: {...user},
   };
   return (
-    <View
+    <ScrollView
       style={{
         flex: 1,
         paddingHorizontal: 20,
@@ -57,57 +64,8 @@ export default function Home(props) {
             flex: 1,
           }}>
           <Form {...formProps} />
-
-          <CTA
-            ctaText={'Logout'}
-            onPress={() => {
-              handleLogout();
-              navigate('Auth');
-            }}
-          />
         </View>
-        {/* <Button
-          title={'Update Profile'}
-          onPress={() => navigate('UpdateProfile')}
-        /> */}
       </View>
-    </View>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  header: {
-    height: 50,
-    justifyContent: 'center',
-  },
-
-  headerText: {
-    fontSize: 25,
-    color: '#362068',
-    fontWeight: '400',
-    fontFamily: 'Helvetica Neue',
-  },
-  footer: {
-    marginTop: 50,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  footerText: {
-    fontSize: 16,
-    fontFamily: 'Helvetica Neue',
-    color: '#636466',
-  },
-
-  footerCTA: {
-    fontSize: 16,
-    color: '#733AC2',
-    fontWeight: '500',
-    fontFamily: 'Helvetica Neue',
-  },
-  errorText: {
-    marginBottom: 8,
-    color: 'red',
-  },
-});
